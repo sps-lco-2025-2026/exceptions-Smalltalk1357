daf5f94940e4fc148ea1146db0092cf10e53a34e
Smalltalk1357 <alex.wan@icloud.com>
2026-05-12 22:19:01 +0100

    Completed up to task 4, debugging task 5

--- exercises/3.finally.cs
+++ exercises/3.finally.cs
@@ -1,2 +1,27 @@
 // exercise 3
-
+Console.Write("Enter a number: ");
+try
+{
+    int n = int.Parse(Console.ReadLine()!);
+    n = n % 2;
+    if (n == 0)
+    {
+        Console.WriteLine("The number is even");
+    }
+    else
+    {
+        Console.WriteLine("The number is odd");
+    }
+}
+catch (FormatException)
+{
+    Console.WriteLine("Error: The value you input is not a valid number")
+}
+catch (Exception)
+{
+    Console.WriteLine("Error: Something unexpected went wrong")
+}
+finally
+{
+    Console.WriteLine("Thank you for using the program")
+}
--- exercises/5.chain.cs
+++ exercises/5.chain.cs
@@ -1,7 +1,15 @@
 
 int Divide(int a, int b)
 {
-    return a / b;
+    int returnValue;
+    try
+    {
+        return = a / b;
+    }
+    catch (DivideByZeroException)
+    { 
+        throw new ArgumentException("Denominator cannot be zero")
+    }
 }
 
 int ReadAndDivide()
@@ -10,7 +18,14 @@ int ReadAndDivide()
     int a = int.Parse(Console.ReadLine()!);
     Console.Write("Denominator: ");
     int b = int.Parse(Console.ReadLine()!);
-    return Divide(a, b);
+    try
+    {
+        return Divide(a, b);
+    }
+    catch (ArgumentException e)
+    {
+        Console.WriteLine("Error: ", e.message);
+    }
 }
 
 Console.WriteLine(ReadAndDivide());
